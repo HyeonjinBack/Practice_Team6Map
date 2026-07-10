bbec7fb67329c35f9e35828166ce6821697bffe0
현진 백 <guswlsrlacl@naver.com>
2026-07-10 15:21:22 +0900

    Initial Commit

---
+++ LiveActivity_Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveActivity_Practice
+//
+//  Created by 백현진 on 7/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveActivity_Practice/LiveActivity_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivity_PracticeApp.swift
+//  LiveActivity_Practice
+//
+//  Created by 백현진 on 7/10/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivity_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
